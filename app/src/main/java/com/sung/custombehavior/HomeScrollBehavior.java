e36c46e565d7b1e5515356270610ab8cefc98a53
sung723 <sung723@outlook.com>
2018-04-24 17:07:59 +0800

    Initial commit

---
+++ app/src/main/java/com/sung/custombehavior/HomeScrollBehavior.java
@@ -0,0 +1,33 @@
+package com.sung.custombehavior;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * Created by sung on 2018/4/24.
+ */
+
+public class HomeScrollBehavior extends CoordinatorLayout.Behavior<TextView> {
+
+    public HomeScrollBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, TextView child, View dependency) {
+        return dependency instanceof Button;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, TextView child, View dependency) {
+        child.setX(dependency.getX()+200);
+        child.setY(dependency.getY()+200);
+        child.setText(dependency.getX()+","+dependency.getY());
+        return true;
+    }
+}
